ad50bb963dd9c8f169d75e09f552c5503d1bd9bb
Deepa <deepajames1199@gmail.com>
2020-07-28 19:42:26 +1000

    Added rolling dice project

---
+++ Dice Rolling Simulator.py
@@ -0,0 +1,14 @@
+## Dice Rolling Simulator ###
+import random
+print("Welcome to the Rolling Dice Simulator")
+
+user = input("Start rolling by pressing Y/N: ").lower()
+while user == 'y':
+    
+    print("You got {}".format(random.randint(1, 7)))
+    user = input("Roll again by pressing Y/N: ").lower()
+
+
+print("Goodbye") if user == 'n' else print('Invalid input')
+
+    
